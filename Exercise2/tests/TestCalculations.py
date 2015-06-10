172c481f5f9e02293fc235af193c1e50a3361a10
Ecki <daneckst@htwg-konstanz.de>
2015-06-10 12:51:00 +0200

    Ordner umbenannt

--- Exercise2/tests/TestCalculations.py
+++ Exercise2/tests/TestCalculations.py
@@ -3,14 +3,14 @@ __author__ = 'Ecki'
 from math import *
 from Exercise2.util import Calculations as Calc
 
-angle1 = -0.69813170079773179 #-10*pi/180
-angle2 = -0.52359877559829882+pi#90*pi/180
+angle1 = -10*pi/180
+angle2 = -11*pi/180
 
-target = 5.6723200689815707
+target = 110*pi/180
 
 #[-0.69813170079773179, -0.52359877559829882, 5.6723200689815707]
 
 print angle1*180/pi
 print angle2*180/pi
-print Calc.get_medial_angle_positive(angle1, angle2, False)*180/pi
-print Calc.get_medial_angle(angle1, angle2, False)*180/pi
+print target*180/pi
+print Calc.angle_in_range(angle1, angle2, target, True)
